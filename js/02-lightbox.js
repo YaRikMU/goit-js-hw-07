b5d3501324dd7ff9bdff5320e35e35657d074f44
Yaroslav Mulyk <Scofild540@gmail.com>
2022-08-31 20:34:35 +0300

    7dz

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallryMarkUp = document.querySelector('.gallery');
+const galleryEl = galleryItems
+.map(({preview, description, original}) =>
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+gallryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt",captionDelay: 250, captionPosition: "bottom",});
